156f08fdd47a0471e1e2a4e52ca42f1ffc572655
Itsrayen <donttouch353@gmail.com>
2026-01-15 17:01:22 +0800

    refactor: Update Vite config to explicitly load environment variables using `loadEnv` based on the current mode.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: process.env.VITE_ALLOWED_HOSTS
-      ? process.env.VITE_ALLOWED_HOSTS.split(',')
-      : ['localhost', '127.0.0.1'],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      allowedHosts: env.VITE_ALLOWED_HOSTS
+        ? env.VITE_ALLOWED_HOSTS.split(',')
+        : ['localhost', '127.0.0.1'],
+    }
   }
 })
